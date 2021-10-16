0cb47769128670b5f5f787018e33150ecddf9521
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-10-16 21:35:15 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:yongDataScince/ts-pop.git master:gh-pages
 
 cd -
