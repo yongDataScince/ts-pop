fa2c546636605b66c3f1ee6e84dac8ea70e9a735
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-10-16 21:19:08 +0300

    comm

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env sh
-# abort on errors
+
+# остановить публикацию при ошибках
 set -e
-# build
-yarn run build
-# navigate into the build output directory
+
+# сборка
+npm run build
+
+# переход в каталог сборки
 cd dist
-# if you are deploying to a custom domain
+
+# если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
