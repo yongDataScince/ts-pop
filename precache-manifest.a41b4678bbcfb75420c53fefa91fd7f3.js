1bf45198e769f76022a1505e97bef52155f59a16
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-10-16 21:52:49 +0300

    add dist

---
+++ precache-manifest.a41b4678bbcfb75420c53fefa91fd7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c853a959b0de40a4fb1",
+    "url": "/ts-pop/css/app.24ec4228.css"
+  },
+  {
+    "revision": "ec0243aa9aca440f1993f91d4a3c0163",
+    "url": "/ts-pop/index.html"
+  },
+  {
+    "revision": "0c853a959b0de40a4fb1",
+    "url": "/ts-pop/js/app.0e7c9c00.js"
+  },
+  {
+    "revision": "172df211134217e893e4",
+    "url": "/ts-pop/js/chunk-vendors.4bb42c57.js"
+  },
+  {
+    "revision": "1124ae1c85cf9bb85143385075957e6d",
+    "url": "/ts-pop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ts-pop/robots.txt"
+  }
+]);
